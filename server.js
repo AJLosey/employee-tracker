905518ac247290a00bd6e7e63a5e6f63425daa6b
AJLosey <aaronlosey11@gmail.com>
2022-05-03 01:51:32 -0500

    wrote functions for querying database, before first testing

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-// Import and require mysql2
+
 const mysql = require('mysql2');
 var inquirer = require('inquirer');
+var cTable = require('console.table');
 
-// Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -63,20 +46,115 @@ inquirer
             updateEmpRole()
         }
     })
-}
+};
 
+const viewDep = function () {
+    db.query(`SELECT * FROM departments`,function (err, results) {
+        console.table(results)
+    });
+inquirerLoop();
+};
 
+const viewRoles = function () {
+    db.query(`SELECT * FROM roles`,function (err, results) {
+        console.table(results)
+    });
+inquirerLoop();
+};
 
-db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+const viewEmp = function () {
+    db.query(`SELECT * FROM roles`,function (err, results) {
+        console.table(results)
+    });
+inquirerLoop();
+};
 
+const addDep = function () {
+    Inquirer
+        .prompt({
+            type: "input",
+            message: "What is the department name?",
+            name: "depName"
+        })
+        .then((answers) => {
+    db.query(`INSERT INTO department (department_name) VALUES (${answers.depName})`,function (err, results) {
+        console.table(results)
+    });
+    inquirerLoop();
+        });
+};
 
-db.query('SELECT * FROM course_names', function (err, results) {
-    console.log(results);
-});
+const addRole = function () {
+    Inquirer
+        .prompt({
+            type: "input",
+            message: "What is the title of the role?",
+            name: "role"
+        },
+        {
+            type: "input",
+            message: "What is the average salary?",
+            name: "salary"
+        },
+        {
+            type: "input",
+            message: "What is the id of the relevant department?",
+            name: "relDep"
+        })
+        .then((answers) => {
+    db.query(`INSERT INTO roles (title, salary, department_id) VALUES (${answers.role}, ${answers.salary}, ${answers.relDep})`,function (err, results) {
+        console.table(results)
+    });
+    inquirerLoop();
+        });
+};
 
+const addEmp = function () {
+    Inquirer
+        .prompt({
+            type: "input",
+            message: "What is their first name?",
+            name: "firstName"
+        },
+        {
+            type: "input",
+            message: "What is their last name?",
+            name: "lastName"
+        },
+        {
+            type: "input",
+            message: "What is the id of their role?",
+            name: "roleId"
+        },
+        {
+            type: "input",
+            message: "What is the id of their manager?",
+            name: "manageId"
+        })
+        .then((answers) => {
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${answers.firstName}, ${answers.lastName}, ${answers.roleId}, ${answers.manageId})`,function (err, results) {
+        console.table(results)
+    });
+    inquirerLoop();
+        });
+};
 
+const updateEmpRole = function () {
+    Inquirer
+        .prompt({
+            type: "input",
+            message: "What is the id of the employee you'd like to update?",
+            name: "empId"
+        },
+        {
+            type: "input",
+            message: "What is the id of their new role?",
+            name: "roleId"
+        })
+        .then((answers) => {
+    db.query(`UPDATE employee SET role_id = ${answers.roleId} WHERE id = ${empId}`,function (err, results) {
+        console.table(results)
+    });
+    inquirerLoop();
+        });
+};
